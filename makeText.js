5edecacd1362241909a19595a7e512dce6dd0a33
alexm <alex-m@outlook.com>
2023-03-23 15:08:39 -0700

    Require axios and fs; add conditionals to check value of relevant command line args; add comments as guidelines for future code

--- makeText.js
+++ makeText.js
@@ -3,8 +3,36 @@
 
 /** Command-line tool to generate Markov text. */
 
+const axios = require("axios");
+const fs = require("fs");
+
 const { MarkovMachine } = require("./markov");
 
 
 const args = process.argv.slice(2);
-const mm = new MarkovMachine("Some random text is here");
+// const mm = new MarkovMachine("Some random text is here");
+
+if (args.length === 2) {
+    const type = args[0];
+    const path = args[1];
+
+    if (type.toLowerCase() === "file") {
+
+        // Read in the file
+
+        // Pass file data to markov to generate text
+
+        // Log the markov text to console
+
+    }
+
+    if (type.toLowerCase() === "url") {
+
+        // Get text data from the given URL (Axios)
+
+        // Pass data to markov to generate text
+
+        // Log the markov text to console
+
+    }
+}
