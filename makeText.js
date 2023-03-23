785f32f0b59f08a095b5e0e6e38f79863a543a0b
alexm <alex-m@outlook.com>
2023-03-23 13:33:10 -0700

    Require markov.js file; get relevant command line args; initialize new MarkovMachine (sample)

--- makeText.js
+++ makeText.js
@@ -3,3 +3,8 @@
 
 /** Command-line tool to generate Markov text. */
 
+const { MarkovMachine } = require("./markov");
+
+
+const args = process.argv.slice(2);
+const mm = new MarkovMachine("Some random text is here");
