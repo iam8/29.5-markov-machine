33e987f8c8d6016911ea3d4281a2a14aa8b9802d
alexm <alex-m@outlook.com>
2023-03-23 22:08:46 -0700

    Create test suite for MarkovMachine constructor; add some tests to the suite

--- markov.test.js
+++ markov.test.js
@@ -5,3 +5,29 @@
 
 const fs = require("fs");
 const { MarkovMachine } = require("./markov");
+
+
+describe("MarkovMachine constructor", function () {
+
+    test("For an empty input text, resulting words list should be empty.", function () {
+        const mm = new MarkovMachine("");
+
+        expect(mm.words).toEqual([]);
+    })
+
+    test("For a nonempty input text, resulting words list should contain the appropriate words.",
+        function () {
+            const mm = new MarkovMachine("Here is some sample text text");
+
+            expect(mm.words).toEqual(["Here", "is", "some", "sample", "text", "text"]);
+        })
+
+    test("Upon initialization, a new MarkovMachine should have a chains property defined",
+        function () {
+            const mm = new MarkovMachine("Sample text");
+
+            expect(mm.chains).toBeDefined();
+        })
+})
+
+
